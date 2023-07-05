86600ab5b05c5288bbf99955fa1c3dd4561b812a
Tatiana Gorkavik <gorkaviktanya@gmail.com>
2023-07-05 20:54:56 +0300

    fix bug with sorting order

--- MatchGame/Assets/MatchGame/Scripts/CellObject.cs
+++ MatchGame/Assets/MatchGame/Scripts/CellObject.cs
@@ -12,12 +12,13 @@ namespace MatchGame
 
         [SerializeField] private CellType _cellType;
         [SerializeField] private Animator _animator;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
 
         private (int X, int Y) _cellCoord;
 
         public CellType CellType => _cellType;
         public (int X, int Y) CellCoord => _cellCoord;
-        
+
         public event Action<CellObject, Vector2> OnPointerUpEvent;
         public event Action<CellObject, Vector2> OnPointerDownEvent;
 
@@ -25,6 +26,8 @@ namespace MatchGame
         {
             transform.position = cellPos;
             _cellCoord = cellPosition;
+
+            _spriteRenderer.sortingOrder = cellPosition.X + cellPosition.Y * 10;
         }
 
         public void OnPointerDown(PointerEventData eventData)
